393ac12cbbadea29a2a32ad0190fa262eca16c9e
BiLQ67 <f.giera-Rodemann@gmx.de>
2022-04-16 18:27:52 +0200

    Pointer_02

---
+++ 02_ArrayPointer/ArrayDifference2.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+
+// int *input_array <=> // int input_array[]
+
+int array_maximum(int *input_array, unsigned int length)
+{
+    int current_max_value = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if(i==0)
+        {
+            current_max_value = input_array[i];
+        }
+        else if(input_array[i] > current_max_value)
+        {
+            current_max_value = input_array[i];
+        }
+    }
+
+
+    return current_max_value;
+}
+
+void print_name(char *name)
+{
+    std::cout << name << std::endl;
+
+}
+
+int main ()
+{
+    unsigned int array_size = 10;
+
+
+    // Heap Allocation
+    int *p = new int[array_size];
+    // Size of the pointer itself in Bytes
+    std::cout << sizeof(p) << std::endl;
+    // Size of the first array element, that pointer points to in Byte
+    std::cout << sizeof(*p) << std::endl;
+
+    for (int i = 0; i < array_size; i++)
+    {
+        p[i] = i;
+    }
+    std::cout << array_maximum(p, array_size) << std::endl;
+
+    int a[5] = { 5, 8, 6, 7, 3 };
+
+    std::cout << array_maximum(a, 5) << std::endl;
+
+    // Heap De-Allocation
+    delete[] p;
+
+    char name[]{"Fabian Giera-Rodemann"};
+    print_name(name);
+
+
+    return 0;
+}
---
+++ 02_ArrayPointer/PointerArray2.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+
+// int *input_array <=> // int input_array[]
+
+int array_maximum(int *input_array, unsigned int length)
+{
+    int current_max_value = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if(i==0)
+        {
+            current_max_value = input_array[i];
+        }
+        else if(input_array[i] > current_max_value)
+        {
+            current_max_value = input_array[i];
+        }
+    }
+
+
+    return current_max_value;
+}
+
+int main ()
+{
+    unsigned int array_size = 10;
+
+
+    // Heap Allocation
+    int *p = new int[array_size];
+    // Size of the pointer itself in Bytes
+    std::cout << sizeof(p) << std::endl;
+    // Size of the first array element, that pointer points to in Byte
+    std::cout << sizeof(*p) << std::endl;
+
+    for (int i = 0; i < array_size; i++)
+    {
+        p[i] = i;
+    }
+    std::cout << array_maximum(p, array_size) << std::endl;
+
+    int a[5] = { 5, 8, 6, 7, 3 };
+
+    std::cout << array_maximum(a, 5) << std::endl;
+
+    // Heap De-Allocation
+    delete[] p;
+
+
+    return 0;
+}
