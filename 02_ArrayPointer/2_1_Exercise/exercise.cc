8af0644390ee6a33d1843c8ad8f9af57612ca3bd
BiLQ67 <f.giera-Rodemann@gmx.de>
2022-04-19 17:24:48 +0200

    Pointer_03

--- 02_ArrayPointer/2_1_Exercise/exercise.cc
+++ 02_ArrayPointer/2_1_Exercise/exercise.cc
@@ -5,9 +5,23 @@
 // Exercise 1
 double array_sum(const double *p_array, const unsigned int &length)
 {
+    double result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        result = result + p_array[i];
+    }
+    return result;
+
 }
 
 // Exercise 2
 int *array_constructor(const int &value, const unsigned int &length)
 {
+    int *array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = value;
+    }
+    return array;
+
 }
--- 02_ArrayPointer/2_2_Exercise/exercise.cc
+++ 02_ArrayPointer/2_2_Exercise/exercise.cc
@@ -5,9 +5,26 @@
 // Exercise 1
 void push_back(int *&input_array, const unsigned int &size, const int &value)
 {
+    int *p = new int[size + 1];
+    for (int i = 0; i < size ; i++)
+    {
+        p[i] = input_array[i];
+    }
+    p[size] = value;
+
+    delete[] input_array;
+    input_array = p;
 }
 
 // Exercise 2
 void pop_back(int *&input_array, const unsigned int &size)
 {
+    int *p = new int[size - 1];
+    for (int i = 0; i < size - 1 ; i++)
+    {
+        p[i] = input_array[i];
+    }
+
+    delete[] input_array;
+    input_array = p;
 }
